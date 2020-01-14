d0adc3aaf972712dd3f4f3c9701e7f151f5cbd96
Kirill Titov <KTitov@croc.ru>
2020-01-14 10:28:40 +0300

    UPDATE: Started to add movement tests
    Cannot properly do it because of regression bug: https://github.com/Unity-Technologies/InputSystem/issues/989
    I have to use workaround or wait until 2019.3 is released.

---
+++ Archero/Assets/Tests/MovementTests/PlayerMovementTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace Tests.MovementTests
+{
+    public class PlayerMovementTests  
+    {
+        private EditorBuildSettingsScene[] scenes;
+
+
+        [SetUp]
+        void Setup()
+        {
+            scenes = EditorBuildSettings.scenes;
+            EditorBuildSettings.scenes = new[]
+            {
+                new EditorBuildSettingsScene("Assets/Tests/Scenes/TestScene", true)
+            };
+        }
+
+
+        [UnityTest]
+        public IEnumerator PlayerMovementTestsWithEnumeratorPasses()
+        {
+            SceneManager.LoadScene("TestScene");
+            yield return null;
+        }
+        
+        [TearDown]
+        void Teardown()
+        {
+            EditorBuildSettings.scenes = scenes;
+        }
+    }
+}
